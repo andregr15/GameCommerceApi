abdbe0ea4454acb7e92237a4392d5965b6bea08f
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-01-06 11:28:23 -0300

    Adding image to product and resolving product factory error

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,9 +3,19 @@ FactoryBot.define do
     sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 5.0...400.0) }
-    
+
+    # class code gives ActiveStorage::IntegridError on tests and seeds
+    # image { Rack::Test::UploadedImage.new(Rails.root.join('spec/support/images/product_image.png')) }
+
     after :build do |product|
       product.productable = create(:game)
+
+      # work around ActiveStorage::IntegridError caused by class code
+      product.image.attach(
+        io: File.open(Rails.root.join('spec/support/images/product_image.png')),
+        filename: 'product_image',
+        content_type: 'image/png'
+      )
     end
   end
 end
